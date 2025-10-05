573492cb475b800b4e109eae6a44e07d6867082f
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-10-05 23:27:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import OmegleWeb from './components/OmegleWeb.jsx';
 import PrivacyPolicy from './components/PrivacyPolicy.jsx';
 import TermsConditions from './components/TermsConditions.jsx';
 import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import NewEra from './components/NewEra.jsx';
+import WelcomeToOmegleWeb from './components/WelcomeToOmegleWeb.jsx';
+import GlobalCommunity from './components/GlobalCommunity.jsx';
+import PrivacyFocusedDesign from './components/PrivacyFocusedDesign.jsx';
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-conditions" element={<TermsConditions />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/new-era" element={<NewEra />} />
+        <Route path="/welcome" element={<WelcomeToOmegleWeb />} />
+        <Route path="/global-community" element={<GlobalCommunity />} />
+        <Route path="/privacy-design" element={<PrivacyFocusedDesign />} />
       </Routes>
     </Router>
   );
